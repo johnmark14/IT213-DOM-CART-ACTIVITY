d3f9a14b1d4c26fcd1abee5672853b556809053d
johnmark14 <johmarkgabrentina@gmail.com>
2021-09-29 09:32:35 +0800

    Added event listener to app.js

---
+++ DOMcart-main/js/app.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function(e) {
+
+    document.querySelector('#clear-cart').addEventListener('click', function() {
+        alert('Hello world!')
+    })
+})
